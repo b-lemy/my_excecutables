34d8fa4af2b2cf1075d7f43ef7c2e8628d330527
b-lemy <lemabrian1234@gmail.com>
2023-03-17 07:40:10 -0700

    python vitual env

---
+++ createVirtualEnv.py
@@ -0,0 +1,10 @@
+## python get-pip.py
+## pip -V
+
+## mkdir <project name>
+
+## cd <project name>
+## py -3 -m venv <name of environment>
+## venv\Scripts\activate
+## pip install Flask
+## Other stuffs to install ....
